36801b819cbfa7b87b90703a1ae39e704f5f6a0e
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-10-14 04:30:45 +0600

    Access form data using onSubmit and e target

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import SimpleFrom from './Components/SimpleFrom/SimpleFrom'
+
+function App() {
+
+
+  return (
+    <>
+     
+      <h1>Explore React From</h1>
+      <SimpleFrom></SimpleFrom>
+    </>
+  )
+}
+
+export default App
